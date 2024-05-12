5967b24266fbfe3ff813c50da5e6b677dd2bb6f5
MiniSpynel <pierrebarrera0@gmail.com>
2024-05-12 04:25:51 +0200

    gravity started, jump bugged

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -31,12 +31,24 @@ var camera_aim_position = 0.6
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity = Vector3(0, -9.81, 0)
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	print(up_direction)
 	
 func _input(event):
+	
+	if event.is_action_pressed("Interact"):
+		if gravity == Vector3(9.81, 0, 0):
+			gravity = Vector3(0, -9.81, 0)
+			rotate_z(deg_to_rad(-90))
+			up_direction = -gravity.normalized()
+		else:
+			gravity = Vector3(9.81, 0, 0)
+			rotate_z(deg_to_rad(90))
+			up_direction = -gravity.normalized()
+	
 	if event is InputEventMouseMotion and not build_menu_interface.visible and not inventory_interface.visible:
 		cameraMountX.rotate_y(deg_to_rad(-event.relative.x * sensitivity_horizontal))
 		cameraMountY.rotation_degrees.x = clamp(cameraMountY.rotation_degrees.x - event.relative.y * sensitivity_vertical, -70, 80)
@@ -63,18 +75,14 @@ func _physics_process(delta):
 		spring_arm_3d.position.x = lerp(spring_arm_3d.position.x, camera_start_position, 5*delta)
 		cursor.hide()
 	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward")
 	var direction = (cameraMountX.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var quat := Quaternion(transform.basis).normalized()
+	var global_direction = quat * direction
+	
 	if direction:
 		if running:
 			if animationPlayer.current_animation != "running":
@@ -82,17 +90,23 @@ func _physics_process(delta):
 		else:
 			if animationPlayer.current_animation != "walking":
 				animationPlayer.play("walking")
-		
-		
-		visuals.look_at(lerp(position - visuals.basis.z, position + direction, 10*delta))
-		
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
 	else:
 		if animationPlayer.current_animation != "idle":
 			animationPlayer.play("idle")
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+
+	#visuals.look_at(lerp(position - visuals.basis.z, position + direction, 10*delta), transform.basis.y)
+	velocity = global_direction * SPEED
+	
+	if not is_on_floor():
+		velocity += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity = JUMP_VELOCITY * -gravity.normalized()
+		
+	if direction:
+		print("direction : %s, global_direction : %s, gravity: %s" % [direction, global_direction, gravity])
 
 	move_and_slide()
 
